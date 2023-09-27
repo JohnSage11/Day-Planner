d675ed5162e94eec2e25023b3e5a0079d7a09a31
JohnSage11 <110984943+JohnSage11@users.noreply.github.com>
2023-09-27 17:20:05 +0100

    Add files via upload

---
+++ control.js
@@ -0,0 +1,30 @@
+function set1(){
+let output1= document.getElementById('out1');
+let input1 = document.getElementById('activity1');
+  output1.innerHTML = input1.value;
+  input1.value='';
+}
+function set2(){
+  let output2= document.getElementById('out2');
+  let input2 = document.getElementById('activity2');
+    output2.innerHTML = input2.value;
+    input2.value='';
+  }
+  function set3(){
+    let output3= document.getElementById('out3');
+    let input3 = document.getElementById('activity3');
+      output3.innerHTML = input3.value;
+      input3.value='';
+    }
+    function set4(){
+      let output4= document.getElementById('out4');
+      let input4 = document.getElementById('activity4');
+        output4.innerHTML = input4.value;
+        input4.value='';
+      }
+      function set5(){
+        let output5= document.getElementById('out5');
+        let input5 = document.getElementById('activity5');
+          output5.innerHTML = input5.value;
+          input5.value='';
+        }
